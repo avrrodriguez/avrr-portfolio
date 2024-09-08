4bd3fb570ed8322ccd0e1f2ef16f0aeae6f65946
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-09-08 17:22:31 -0400

    adds site colors, top section with h1 and h2

---
+++ src/app/topics/TopSection.tsx
@@ -0,0 +1,10 @@
+import "./top-section.css";
+
+export default function TopSection() {
+  return(
+    <div className="top-section">
+      <h1>Abraham V Rodriguez Rodriguez</h1>
+      <h2>Front End Developer</h2>
+    </div>
+  )
+}
