dda4c777d2bca36abe5ccc03f4033fbbc40117b3
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-09-08 22:00:50 -0400

    add ripple effect div

--- src/app/topics/TopSection.tsx
+++ src/app/topics/TopSection.tsx
@@ -1,8 +1,10 @@
 import "./top-section.css";
+import "../effects/ripple-effect.css";
 
 export default function TopSection() {
   return(
-    <div className="top-section">
+    <div className="top-section ripple-animation">
+      <div className="ripple-effect"></div>
       <h1>Abraham V Rodriguez Rodriguez</h1>
       <h2>Front End Developer</h2>
     </div>
