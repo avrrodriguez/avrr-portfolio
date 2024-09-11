acfae65f8605a4282eb5cb08cecd0c34f2b364a9
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-09-10 22:11:16 -0400

    adds images on the top section

--- src/app/topics/TopSection.tsx
+++ src/app/topics/TopSection.tsx
@@ -1,5 +1,10 @@
 import "./top-section.css";
 import "../effects/ripple-effect.css";
+import book from '../../../images/book-image.png';
+import gear from '../../../images/gear-image.png';
+import chess from '../../../images/chess-piece-knight.png';
+import computer from '../../../images/computer-pencil-designs-image.png';
+import brush from '../../../images/paintbrush-image-edited.png';
 
 export default function TopSection() {
   return(
@@ -8,6 +13,11 @@ export default function TopSection() {
         <h1>Abraham V Rodriguez Rodriguez</h1>
         <h2>Front End Developer</h2>
       </div>
+      <img className="book-image" src={book.src} alt="book image"/>
+      <img className="chess-piece-image" src={gear.src} alt="chess piece image"/>
+      <img className="computer-pencil-image" src={chess.src} alt="computer ipad phone image"/>
+      <img className="gear-image" src={computer.src} alt="gear image"/>
+      <img className="paintbrush-image" src={brush.src} alt="paintbrush image"/>
     </div>
   )
 }
