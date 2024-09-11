cc5f9b642018e6bdecedb69d9c55c0f1cde57ce7
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-09-10 20:59:46 -0400

    adds images, fixes the entrance animation

--- src/app/topics/TopSection.tsx
+++ src/app/topics/TopSection.tsx
@@ -3,10 +3,11 @@ import "../effects/ripple-effect.css";
 
 export default function TopSection() {
   return(
-    <div className="top-section ripple-animation">
-      <div className="ripple-effect"></div>
-      <h1>Abraham V Rodriguez Rodriguez</h1>
-      <h2>Front End Developer</h2>
+    <div className="top-section ripple-effect">
+      <div className="top-section-content">
+        <h1>Abraham V Rodriguez Rodriguez</h1>
+        <h2>Front End Developer</h2>
+      </div>
     </div>
   )
 }
