b5923fac5d3c01e1a41eec4dbde493c39f4ee5ec
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-10-12 17:58:14 -0400

    adds structure and some design to web developer work experience

--- src/app/topics/WorkExperience.tsx
+++ src/app/topics/WorkExperience.tsx
@@ -24,8 +24,50 @@ export default function WorkExperience() {
         />
       </div>
       <div className="web-developer-info">
-        <div className="web-developer-tools">Languages/tools</div>
-        <div className="web-developer-skills">Skills</div>
+        <div className="web-developer-tools">
+          <div className="web-developer-tools-title">
+            <h4>Technologies</h4>
+            <Image 
+              className="web-developer-subtitle-accent-image" 
+              src='/subheadings-accent-img.png' 
+              alt="main headers image" 
+              width={300} 
+              height={300} 
+            />
+          </div>
+          <div className="web-developer-tools-info">
+            <div className="web-developer-tools-programming">
+              <p>Wordpress, Oxygen, Breakdance, Elementor, Cornerstone, plugins</p>
+              <p>HTML, CSS, Javascript, PHP, jQuery</p>
+            </div>
+            <div className="web-developer-tools-general">
+              <p>Adobe Stuff</p>
+              <p>Cloud Storage and website servers. Version control (i.e. UpdraftPlus)</p>
+            </div>
+          </div>
+        </div>
+        <div className="web-developer-skills">
+          <div className="web-developer-skills-title">
+            <h4>Skills</h4>
+            <Image 
+              className="web-developer-subtitle-accent-image"
+              src='/subheadings-accent-img.png' 
+              alt="main headers image" 
+              width={300} 
+              height={300} 
+            />
+          </div>
+          <div className="web-developer-skills-info">
+            <div className="web-developer-skills-websites">
+              <p>SEO something something something something</p>
+              <p>Accessability</p>
+            </div>
+            <div className="web-developer-skills-social">
+              <p>Client Interation something something something something</p>
+              <p>Teamwork</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
