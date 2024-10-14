c3a3e6a498ecbd7bba39b5d63fcc9659e99a44d8
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-10-13 21:14:15 -0400

    adds to web developer experience and adds front end developer work experience

--- src/app/topics/WorkExperience.tsx
+++ src/app/topics/WorkExperience.tsx
@@ -13,58 +13,123 @@ export default function WorkExperience() {
           width={300} 
           height={300} 
         />
-      <div className="web-developer-title">
+      <div className="work-experience-title">
         <h3>Web Developer</h3>
         <Image 
-          className="web-developer-title-accent-image" 
+          className="work-experience-title-accent-image" 
           src='/main-headers-accent-img.png' 
           alt="main headers image" 
           width={300} 
           height={300} 
         />
+        <p className="work-experience-description">Description</p>
       </div>
-      <div className="web-developer-info">
-        <div className="web-developer-tools">
-          <div className="web-developer-tools-title">
+      <div className="work-experience-info">
+        <div className="work-experience-tools">
+          <div className="work-experience-tools-title">
             <h4>Technologies</h4>
             <Image 
-              className="web-developer-subtitle-accent-image" 
+              className="work-experience-subtitle-accent-image" 
               src='/subheadings-accent-img.png' 
               alt="main headers image" 
               width={300} 
               height={300} 
             />
           </div>
-          <div className="web-developer-tools-info">
-            <div className="web-developer-tools-programming">
-              <p>Wordpress, Oxygen, Breakdance, Elementor, Cornerstone, plugins</p>
-              <p>HTML, CSS, Javascript, PHP, jQuery</p>
+          <div className="work-experience-tools-info">
+            <div className="work-experience-tools-programming">
+              <p>Designed, managed, and built 100+ sites in wordpress using multiple builders.</p>
+              <p>Used HTML, CSS, Javascript, PHP, and jQuery every day in order to build sites that satistfy clients.</p>
             </div>
-            <div className="web-developer-tools-general">
-              <p>Adobe Stuff</p>
-              <p>Cloud Storage and website servers. Version control (i.e. UpdraftPlus)</p>
+            <div className="work-experience-tools-general">
+              <p>Proficient in Adobe Creative Cloud to create visually compelling designs, and multimedia content.</p>
+              <p>Aided in the use of cloud storage as a server manager to host 100+ sites, as well as Updraft Pro for version control.</p>
             </div>
           </div>
         </div>
-        <div className="web-developer-skills">
-          <div className="web-developer-skills-title">
+        <div className="work-experience-skills">
+          <div className="work-experience-skills-title">
             <h4>Skills</h4>
             <Image 
-              className="web-developer-subtitle-accent-image"
+              className="work-experience-subtitle-accent-image"
               src='/subheadings-accent-img.png' 
               alt="main headers image" 
               width={300} 
               height={300} 
             />
           </div>
-          <div className="web-developer-skills-info">
-            <div className="web-developer-skills-websites">
-              <p>SEO something something something something</p>
-              <p>Accessability</p>
+          <div className="work-experience-skills-info">
+            <div className="work-experience-skills-websites">
+              <p>Implemented data-driven search engine optimization (SEO) techniques, including keyword research (relevant keywords) and content optimization(meta data) strategies.</p>
+              <p>Ensured digital accessability compliance (WCAG 2.1) across web content, enhancing user experience for individuals with disabilities and improving overall site usability.</p>
             </div>
-            <div className="web-developer-skills-social">
-              <p>Client Interation something something something something</p>
-              <p>Teamwork</p>
+            <div className="work-experience-skills-social">
+              <p>Managed client relationships, and provided tailored solutions. Conducted regular communications to understand needs, deliver progress updates, and ensure project alignment.</p>
+              <p>Collaborated effectively within cross-functional teams, contributing to brainstorming sessions, project planning and problem-solving.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Image 
+          className="work-experience-transition-image" 
+          src='/transition-accent-img.png' 
+          alt="transition paint stroke accent image" 
+          width={300} 
+          height={300} 
+        />
+      <div className="work-experience-title">
+        <h3>Front End Developer</h3>
+        <Image 
+          className="work-experience-title-accent-image" 
+          src='/main-headers-accent-img.png' 
+          alt="main headers image" 
+          width={300} 
+          height={300} 
+        />
+        <p className="work-experience-description">Description</p>
+      </div>
+      <div className="work-experience-info">
+        <div className="work-experience-tools">
+          <div className="work-experience-tools-title">
+            <h4>Technologies</h4>
+            <Image 
+              className="work-experience-subtitle-accent-image" 
+              src='/subheadings-accent-img.png' 
+              alt="main headers image" 
+              width={300} 
+              height={300} 
+            />
+          </div>
+          <div className="work-experience-tools-info">
+            <div className="work-experience-tools-programming">
+              <p>Designed, managed, and built a professional website using Next.js/React.js</p>
+              <p>Used HTML, CSS, Javascript, and Typescript in order to build the front end of the website.</p>
+            </div>
+            <div className="work-experience-tools-general">
+              <p>Proficient in Adobe Creative Cloud to create visually compelling designs, and multimedia content.</p>
+              <p>Used Vercel as a server manage and Github for version control.</p>
+            </div>
+          </div>
+        </div>
+        <div className="work-experience-skills">
+          <div className="work-experience-skills-title">
+            <h4>Skills</h4>
+            <Image 
+              className="work-experience-subtitle-accent-image"
+              src='/subheadings-accent-img.png' 
+              alt="main headers image" 
+              width={300} 
+              height={300} 
+            />
+          </div>
+          <div className="work-experience-skills-info">
+            <div className="work-experience-skills-websites">
+              <p>Implemented data-driven search engine optimization (SEO) techniques, including keyword research (relevant keywords) and content optimization(meta data) strategies.</p>
+              <p>Ensured digital accessability compliance (WCAG 2.1) across web content, enhancing user experience for individuals with disabilities and improving overall site usability.</p>
+            </div>
+            <div className="work-experience-skills-social">
+              <p>Managed client relationship, and provided tailored solutions. Conducted regular communications to understand needs, deliver progress updates, and ensure project alignment.</p>
+              <p>Spear headed and successfully completed a full-scale website independently, from designing, build, and launch, demonstrating strong time-management, problem solving, and decision making skills.</p>
             </div>
           </div>
         </div>
