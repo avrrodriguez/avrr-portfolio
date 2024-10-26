9424598a0609f81249cf6ec426144cea8b3c3f6c
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-10-26 08:17:50 -0400

    fixes build errors

--- src/app/utils/ScrollViewportChecker.tsx
+++ src/app/utils/ScrollViewportChecker.tsx
@@ -1,13 +1,13 @@
 export default function ScrollViewportChecker(elemClassName: string) {
 
-  let win: HTMLElement | null = document.getElementById(elemClassName);
-  let elemInViewPort: DOMRect | undefined = win?.getBoundingClientRect();
+  const win: HTMLElement | null = document.getElementById(elemClassName);
+  const elemInViewPort: DOMRect | undefined = win?.getBoundingClientRect();
 
   if (elemInViewPort) {
       return (
         elemInViewPort.top + 128 <= (window.innerHeight || document.documentElement.clientHeight) &&
         elemInViewPort.left >= 0 &&
-        elemInViewPort.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+        elemInViewPort.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
 };
