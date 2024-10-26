e1675f70b798a7c7a5f9295a69d00df0c4020157
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-10-26 15:42:15 -0400

    adds a hamburger manu and replaces with normal header menu on lower widths

--- src/app/topics/Header.tsx
+++ src/app/topics/Header.tsx
@@ -1,9 +1,24 @@
 "use client"
 
 import "../styles/header.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [hamToggle, setHamToggle] = useState(false);
+
+  function handleClickHamburgerMenu() {
+    if (hamToggle) {
+      setHamToggle(false);
+      document.getElementsByClassName("hamburger-toggle")[0].classList.remove("toggle-true");
+      document.getElementsByClassName("hamburger-menu")[0].classList.remove("menu-toggle-true");
+    } else {
+      setHamToggle(true);
+      document.getElementsByClassName("hamburger-toggle")[0].classList.add("toggle-true");
+      document.getElementsByClassName("hamburger-menu")[0].classList.add("menu-toggle-true");
+    }
+
+    console.log(typeof hamToggle, hamToggle);
+  }
 
   function handleClickHeaderMenu(element: Element, allElements: HTMLCollectionOf<Element>): void {
 
@@ -32,6 +47,20 @@ export default function Header() {
 
   return (
     <header className="header">
+      <div className="header-menu-hamburger" onClick={handleClickHamburgerMenu}>
+        <div className="hamburger-toggle">
+          <div className="hamburger-toggle-line"></div>
+          <div className="hamburger-toggle-line"></div>
+          <div className="hamburger-toggle-line"></div>
+        </div>
+        <div className="hamburger-menu">
+          <a className="header-menu-link" href="/#about-me">About Me</a>
+          <a className="header-menu-link" href="/#work-experience">Work Experiences</a>
+          <a className="header-menu-link" href="/#web-developer">Web Developer</a>
+          <a className="header-menu-link" href="/#front-end-developer">Front End Developer</a>
+          <a className="header-menu-link" href="/#contact-section">Contact Me</a>
+        </div>
+      </div>
       <div className="header-menu">
         <a className="header-menu-link" href="/#about-me">About Me</a>
         <a className="header-menu-link" href="/#work-experience">Work Experiences</a>
